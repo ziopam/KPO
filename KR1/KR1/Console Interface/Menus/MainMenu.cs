cae7c45ed7be97605efcf722bb4925c0ac9ccc88
ziopam <ppd.orel@mail.ru>
2025-03-20 18:09:00 +0300

    Added file export and readme

--- KR1/KR1/Console Interface/Menus/MainMenu.cs
+++ KR1/KR1/Console Interface/Menus/MainMenu.cs
@@ -9,6 +9,7 @@ namespace KR1.Console_Interface.Menus
         private readonly Menu showMenu;
         private readonly Menu deleteMenu;
         private readonly Menu editMenu;
+        private readonly Menu exportMenu;
 
         public MainMenu(IFinanceDatabase financeDatabase)
         {
@@ -16,11 +17,13 @@ namespace KR1.Console_Interface.Menus
             showMenu = new ShowMenu(financeDatabase, this);
             deleteMenu = new DeleteMenu(financeDatabase, this);
             editMenu = new EditMenu(financeDatabase, this);
+            exportMenu = new ExportMenu(financeDatabase, this);
 
             AddItem(new MenuItem { Title = "Добавить в базу данных", Command = new OpenMenuCommand(addMenu) });
             AddItem(new MenuItem { Title = "Показать базу данных", Command = new OpenMenuCommand(showMenu) });
             AddItem(new MenuItem { Title = "Удалить из базы данных", Command = new OpenMenuCommand(deleteMenu) });
             AddItem(new MenuItem { Title = "Редактировать базу данных", Command = new OpenMenuCommand(editMenu) });
+            AddItem(new MenuItem { Title = "Экспорт базы данных", Command = new OpenMenuCommand(exportMenu) });
         }
     }
 }
---
+++ KR1/KR1/Export/DataExporterTemplate.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+internal abstract class DataExporterTemplate<T>
+{
+    public string FilePath { get; set; } = $"{typeof(T).Name}";
+
+    public void Export(List<T> data)
+    {
+        string exportedData = SerializeData(data);
+        SaveToFile(exportedData);
+    }
+
+    protected abstract string SerializeData(List<T> data);
+
+    protected virtual void SaveToFile(string data)
+    {
+        try
+        {
+            System.IO.File.WriteAllText(FilePath, data, Encoding.UTF8);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Ошибка при сохранении данных в файл: {e.Message}");
+            Environment.Exit(1);
+        }
+    }
+}
